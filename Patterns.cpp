9ba9d20579ce2134713d5ca43e55ff03d8e388f2
Zaid786-Student <aapka-github-email@gmail.com>
2026-03-12 06:26:08 +0530

    Paterns Learnig

--- Patterns.cpp
+++ Patterns.cpp
@@ -41,16 +41,74 @@
 **
 *                                                                   */
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 4;
+
+//     for(int i = 1; i<=n; i++){
+//         for(int j=1; j <=(n-i+1); j++){
+//             cout  << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//                                STAR PATTERN
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 4;
+//     for(int i = 1; i <=n; i++){
+//         for(int j = 1; j<=i; j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//                                  HALF PYRAMID PATTERNS
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     char ch = 'A';
+//     int n = 5;
+//     for(int i = 1; i <=n; i++){
+//         for(int j = 1; j<=i; j++){
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+/*            PATTERNS FOR CHARACTER PYRAMID 
+A
+BC
+DEF
+GHIJ
+*/
+
 #include <iostream>
 using namespace std;
-
 int main() {
     int n = 4;
 
     for(int i = 1; i<=n; i++){
-        for(int j=1; j <=(n-i+1); j++){
-            cout  << "* ";
+        cout << "*"; // first star
+        for(int j = 1; j <= n-1; j++){
+            if(i == 1 || i== n) {
+                cout << "*";
+            } else{
+                cout << " ";
+            }
         }
-        cout << endl;
+        cout << "*" << endl; // last star
     }
+    return 0; 
 }
