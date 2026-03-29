f68f199f7f548f9c5a19355bae08335c748bdddd
Zaid786-Student <aapka-github-email@gmail.com>
2026-03-29 14:12:46 +0530

    Pattern ending

--- Patterns.cpp
+++ Patterns.cpp
@@ -94,21 +94,103 @@ DEF
 GHIJ
 */
 
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 4;
+
+//     for(int i = 1; i<=n; i++){
+//         cout << "*"; // first star
+//         for(int j = 1; j <= n-1; j++){
+//             if(i == 1 || i== n) {
+//                 cout << "*";
+//             } else{
+//                 cout << " ";
+//             }
+//         }
+//         cout << "*" << endl; // last star
+//     }
+//     return 0; 
+// }
+//                                Hollow Rectangle Pattern
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 10;
+//     for(int i = 1; i<=n; i++){
+//         //spaces
+//         for(int j = 1; j<=(n-i); j++){
+//             cout << " ";
+//         }
+
+//         // stars
+//         for(int j=1; j<=i; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//  return 0;
+// }
+//                          Inverted star pyramid
+/*
+     *
+    **
+   ***
+  ****
+ *****
+******
+
+*/
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int num = 1;
+//     int n = 10;
+//     for(int i = 1; i<=n; i++){
+//         for(int j = 1; j<=i; j++){
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+//        FLOYD'S TRIANGLE
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
 #include <iostream>
 using namespace std;
 int main() {
     int n = 4;
-
-    for(int i = 1; i<=n; i++){
-        cout << "*"; // first star
-        for(int j = 1; j <= n-1; j++){
-            if(i == 1 || i== n) {
-                cout << "*";
-            } else{
-                cout << " ";
-            }
+    // 1st Pyramid upward
+    for(int i = 1; i<=4; i++){
+        // space
+        for(int j = 1; j<=(n-i); j++){
+            cout << " ";
         }
-        cout << "*" << endl; // last star
+        // stars
+        for(int j = 1; j<= (2*i-1); j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    //2nd pyramid
+    for(int i =n; i>=1; i--){
+        //star
+        for(int j= 1; j<=(n-i); j++){
+            cout << " ";
+        }
+        for(int j = 1; j<=(2*i-1); j++){
+            cout << "*";
+        }
+        cout << endl;
     }
     return 0; 
 }
