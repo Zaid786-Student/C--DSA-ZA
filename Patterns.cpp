63418a5ab94e812d197dedff37fcd038ea408d45
Zaid786-Student <aapka-github-email@gmail.com>
2026-04-05 14:47:35 +0530

    Pattern end

--- Patterns.cpp
+++ Patterns.cpp
@@ -165,32 +165,88 @@ GHIJ
 11 12 13 14 15
 */
 
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 4;
+//     // 1st Pyramid upward
+//     for(int i = 1; i<=4; i++){
+//         // space
+//         for(int j = 1; j<=(n-i); j++){
+//             cout << " ";
+//         }
+//         // stars
+//         for(int j = 1; j<= (2*i-1); j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     //2nd pyramid
+//     for(int i =n; i>=1; i--){
+//         //star
+//         for(int j= 1; j<=(n-i); j++){
+//             cout << " ";
+//         }
+//         for(int j = 1; j<=(2*i-1); j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0; 
+// }
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n=4;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << " ";
+//         }
+//         cout << "\\";
+//         for(int j=1; j<=2*(n-i); j++){
+//             cout << " ";
+//         }
+//         cout << "/";
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+/*
+\      /
+ \    /
+  \  /
+   \/
+*/
+
 #include <iostream>
 using namespace std;
 int main() {
-    int n = 4;
-    // 1st Pyramid upward
-    for(int i = 1; i<=4; i++){
-        // space
-        for(int j = 1; j<=(n-i); j++){
+    int n = 10; 
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<= i; j++){
+            cout << "*";
+        }
+        for(int j=1; j<=2*(n-i); j++){
             cout << " ";
         }
-        // stars
-        for(int j = 1; j<= (2*i-1); j++){
+        for(int j=1; j<=i; j++){
             cout << "*";
         }
         cout << endl;
     }
-    //2nd pyramid
-    for(int i =n; i>=1; i--){
-        //star
-        for(int j= 1; j<=(n-i); j++){
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=i; j--){
+            cout << "*";
+        }
+        for(int j=1; j<=2*(i-1); j++){
             cout << " ";
         }
-        for(int j = 1; j<=(2*i-1); j++){
+        for(int j=n; j>=i; j--){
             cout << "*";
         }
         cout << endl;
     }
     return 0; 
 }
+//                        BUTTERFLY PATTERN
